6141234732d8e80738bf0eae3bb43974abdc2a6b
Philip Reji Rajan <philiprejirajan@gmail.com>
2025-05-24 17:16:25 +0530

    Change existing files.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
-class name {
-    constructor(parameters) {
-        
-    }
-}
+const body = document.getElementById("body");
+const div = document.getElementById("sample");
+
+body.addEventListener("click", () => {
+    const newColor = randomColor();
+    document.body.style.backgroundColor = newColor;
+});
+
+div.addEventListener("click", () => {
+    const newColor = randomColor();
+    div.style.backgroundColor = newColor;
+});
+
+let newColor;
+
+const randomColor = () => {
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
+    newColor = `rgb(${r}, ${g}, ${b})`;
+    return newColor;
+};
